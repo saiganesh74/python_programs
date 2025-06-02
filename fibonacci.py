b31a4f6ae991af34e8a9582a8274b9311c79cc18
saiganesh74 <ganeshrejeti1@gmail.com>
2025-06-02 18:02:28 +0530

    Deleted

--- fibonacci.py
+++
@@ -1,9 +0,0 @@
-# 0 1 1 2 3 5 8 
-n = int(input("Enter the number : "))
-class Fibo():
-    def __init__(self,n):
-        self.n = n 
-    def fibonacci(self):
-        print(self.n + (self.n-1))
-f = Fibo(n)
-f.fibonacci()
