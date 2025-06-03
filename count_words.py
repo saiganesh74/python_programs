6d775118e6b860e08da8d6b68a38905d8a9743b7
saiganesh74 <ganeshrejeti1@gmail.com>
2025-06-03 09:20:10 +0530

    Couting words

---
+++ count_words.py
@@ -0,0 +1,9 @@
+a = str(input("Enter a sentence to check how many words are there :"))
+class Count_words():
+    def __init__(self,a ):
+        self.a = a
+    def count(self):
+        count = a.split()
+        print(len(count))
+c = Count_words(a);
+c.count()
