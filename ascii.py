f3dd3b7a36e8beccaedc09fb6998687ab424b1ab
saiganesh74 <ganeshrejeti1@gmail.com>
2025-06-03 10:24:20 +0530

    Updated ascii

--- ascii.py
+++ ascii.py
@@ -1,9 +1,9 @@
-ch = input("Enter a character to know the value in ASCII :")
+ch = input("Enter a character to know the value in ASCII :")[0]
 class ASC():
     def __init__(self,ch):
         self.ch = ch
     def converter(self):
         res =ord(self.ch)
-        print(f"The ASCII value of entered character is :{res}")
+        print(f"The ASCII value of {self.ch} is :{res}")
 a = ASC(ch)
 a.converter()
