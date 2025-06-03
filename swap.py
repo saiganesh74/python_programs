c6c668a66ca13e701919ef3bf0a15b9f8485f0ce
saiganesh74 <ganeshrejeti1@gmail.com>
2025-06-03 15:12:02 +0530

    Swapping 2 numbers

--- swap.py
+++ swap.py
@@ -1,17 +1,12 @@
-a = int(input  ("Enter first value :"))
-b = int(input("Enter second value :"))
-
-class swap():
+a = int(input("Enter first number to swap :"))
+b = int(input("Enter second number to swap :"))
+print(f"The values before swapping are {a} and {b}")
+class Swapper():
     def __init__(self,a,b):
-        self.a = a 
+        self.a = a
         self.b = b
-        print(f"First value {self.a}")
-        print(f"Second value {self.b}")
-    def log(self):
-        temp = self.a
-        self.a = self.b 
-        self.b = temp
-        print(self.a)
-        print(self.b)
-s = swap(a,b)
-s.log()
+    def swapping(self):
+        self.a , self.b = self.b , self.a
+        print(f"The values after swapping are {self.a} and {self.b}")
+s = Swapper(a,b)
+s.swapping()
