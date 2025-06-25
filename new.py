6e6d7df9518e071084941a388ce1219cfded9104
saiganesh74 <ganeshrejeti1@gmail.com>
2025-06-25 15:22:08 +0530

    NLP basic stem

---
+++ new.py
@@ -0,0 +1,7 @@
+from nltk.stem import PorterStemmer
+stemmer = PorterStemmer()
+text = 'running , runner , ran'
+words = text.split(",")
+stemmed_words = [stemmer.stem(word) for word in words ]
+text = ".".join(stemmed_words)
+print(text)
