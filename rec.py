c0de24b1b77c3b14f16b248d3d1a5c6fef53c323
saiganesh74 <ganeshrejeti1@gmail.com>
2025-07-10 23:14:42 +0530

    rectangle based

---
+++ rec.py
@@ -0,0 +1,17 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+
+# Create an object of Rectangle
+rect = Rectangle(5, 3)
+
+print("Area:", rect.area())
+print("Perimeter:", rect.perimeter())
